83975031da68203bf06a678d0ad9b1eb2e651faf
zhangyumeng1sjtu <zhangyumeng_sjtu@163.com>
2021-08-15 21:41:16 +0800

    Change hhblits.sh

--- hhblits.sh
+++ hhblits.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for pdb_dir in `ls data/pdbs/demo/*`
+for pdb_dir in data/pdbs/demo/*
 do
+    [[ -e $pdb_dir ]]
     python ThermoGNN/tools/hhblits.py -i $pdb_dir \
                                       -db ../hhsuite_db/UniRef30_2020_06 \
                                       -o data/hhm/demo/ \
